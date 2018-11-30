592f8de26cf4d437c1e1123682c4905d573ca48f
sy <3302274278@qq.com>
2018-11-30 17:42:12 +0800

    添加方法

--- system-dao/src/test/java/DomainTest.java
+++ system-dao/src/test/java/DomainTest.java
@@ -5,5 +5,9 @@ public class DomainTest {
     public void Test(){
         user user1 = new user(1,"admin","123");
         System.out.println(user1);
+
+    }
+    public void strTest(){
+        System.out.println("aaaaaaa");
     }
 }
